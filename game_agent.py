e6bf09658f2bb32a4266a52ceb376c65b730f8ea
Wanda W <wanda.yanli@gmail.com>
2017-02-18 19:02:00 -0500

    Update game_agent.py

--- game_agent.py
+++ game_agent.py
@@ -133,7 +133,10 @@ class CustomPlayer:
         # Perform any required initializations, including selecting an initial
         # move from the game board (i.e., an opening book), or returning
         # immediately if there are no legal moves
-
+        
+        if not legal_moves: #len(legal_moves) == 0:
+            return (-1, -1)
+        
         try:
             # The search method call (alpha beta or minimax) should happen in
             # here in order to avoid timeout. The try/except block will
